dbbb090573097dbdb554240628f6e27fb4493a6f
comedu <comeduschool@gmail.com>
2022-02-10 14:47:29 +0900

    Update and Create new changes

---
+++ buy.py
@@ -0,0 +1,2 @@
+def buy(item):
+    print(f"Get item (f{item}")
--- talk.py
+++ talk.py
@@ -1,3 +1,4 @@
 def talk(src, dest):
     print(f"{src}: blah, blah")
     print(f"{dest}: vividi badidi")
+    print(f"{src} and {dest} are talking...")
